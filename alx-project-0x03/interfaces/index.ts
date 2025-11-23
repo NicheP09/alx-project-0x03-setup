326382085e7b13cf46e91e00446c8076b644ad41
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-23 16:35:39 +0100

    Add index.ts to interface and move all interface there

---
+++ alx-project-0x03/interfaces/index.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
+  action?: () => void;
+}
